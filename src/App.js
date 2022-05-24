422f25949feb97a9582713d97318c0079c8cd986
Heet <senghaniheet40@gmail.com>
2022-05-24 16:28:54 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 
 function App() {
+  const [people,setPeople] = useState(data);
+  const [index,setIndex] = useState(0);
+
+  const nextSlide = () => {
+    setIndex((oldIndex)=>{
+      let index = oldIndex + 1;
+      if (index > data.length-1) {
+        index=0;
+      }
+      return index;
+    })
+  }
+  const prevSlide = () => {
+    setIndex((oldIndex)=>{
+      let index = oldIndex - 1;
+      if (index < 0) {
+        index=data.length-1;
+      }
+      return index;
+    })
+  }
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+       setIndex((oldIndex) => {
+         let index = oldIndex + 1;
+         if (index > data.length - 1) {
+           index = 0;
+         }
+         return index;
+       });
+    }, 5000);
+  
+    return () => {
+      clearInterval(slider);
+    }
+  }, [index])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='section'>
+      <div className="title">
+        <h2>
+          <span>/</span>
+          "reviews"
+        </h2>
+      </div>
+      <div className="section-center">
+        {
+          people.map((person,personIndex)=>{
+            const {id,image,name,title,quote} = person;
+
+            let position = "nextSlide";
+            if (personIndex===index) {
+              position = "activeSlide";
+            }
+            if(personIndex === index-1 || personIndex===people.length-1 && index===0){
+              position="lastSlide";
+            }
+
+            return(
+              <>
+              <article className={position} key={id}>
+                <img src={image} className="person-img" alt={name} />
+                <h4>{name}</h4>
+                <p className="title">{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon"/>
+              </article>
+              <button className="prev" onClick={prevSlide}><FiChevronLeft/></button>
+              <button className="next" onClick={nextSlide}><FiChevronRight/></button>
+              </>
+            )
+          })
+        }
+      </div>
+    </section>
+    
   );
 }
 
